3335217436072888441293c57095a77a413660f0
Prasanth0504 <balaprasanth131@gmail.com>
2023-11-06 13:11:47 +0530

    mouse

---
+++ src/com/HoverPractice/HoverClass.java
@@ -0,0 +1,40 @@
+package com.HoverPractice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class HoverClass {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("WebDriver.chrome.driver","C:\\Users\\Prasanth\\eclipse-workspace\\SeleniumTesting\\src\\com\\FirstClass\\SeleniumPractice.java");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.flipkart.com/");
+		driver.manage().window().maximize();
+		
+		WebElement Xmark = driver.findElement(By.xpath("//button[@class='_2KpZ6l _2doB4z']"));
+		Xmark.click();
+		
+		WebElement Electronics = driver.findElement(By.xpath("//div[text()='Electronics']"));
+	
+		Actions actions = new Actions (driver);
+		actions.moveToElement(Electronics).perform();
+		
+		WebElement Gaming = driver.findElement(By.xpath("//a[text()='Gaming']"));
+		actions.moveToElement(Gaming).perform();
+		WebElement Gamepad = driver.findElement(By.xpath("//a[text()='Gamepads']"));
+		Gamepad.click();
+		
+		
+		
+		
+		
+
+	}
+
+}
